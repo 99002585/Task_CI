4ae1821c0fc54da02bc6ec62f2f577bdd98340f5
99002585 <71482380+99002585@users.noreply.github.com>
2020-09-19 01:49:15 +0530

    Update test_calculator.c

--- test/test_calculator.c
+++ test/test_calculator.c
@@ -2,7 +2,7 @@
 #include <CUnit/CUnit.h>
 
 /* Modify these two lines according to the project */
-#include <calculator_operations.h>
+#include <calculator.h>
 #define PROJECT_NAME    "Calculator"
 
 /* Prototypes for all the test functions */
@@ -10,6 +10,18 @@ void test_add(void);
 void test_subtract(void);
 void test_multiply(void);
 void test_divide(void);
+void test_factorial(void);
+void test_evenodd(void);
+void test_angstrom(void);
+void test_prime(void);
+void test_circlearea(void);
+void test_circlepm(void);
+void test_squarearea(void);
+void test_squarepm(void);
+void test_rectarea(void);
+void test_rectpm(void);
+void test_triarea(void);
+void test_tripm(void);
 
 /* Start of the application test */
 int main() {
@@ -26,8 +38,19 @@ int main() {
   CU_add_test(suite, "subtract", test_subtract);
   CU_add_test(suite, "multiply", test_multiply);
   CU_add_test(suite, "divide", test_divide);
-
-
+  CU_add_test(suite,5,test_factorial );
+    CU_add_test(suite,6,test_angstrom );
+    CU_add_test(suite,7,test_prime );
+    CU_add_test(suite,8,test_evenodd );
+    CU_add_test(suite,9,test_circlearea );
+    CU_add_test(suite,10,test_circlepm );
+    CU_add_test(suite,11,test_squarearea );
+    CU_add_test(suite,12,test_squarepm);
+    CU_add_test(suite,13,test_rectarea );
+    CU_add_test(suite,14,test_rectpm );
+    CU_add_test(suite,15,test_triarea );
+    CU_add_test(suite,16,test_tripm );
+  
 /* Note: Do not edit START*/
   /* Setup Test Framework to output the result to Screen */
   CU_basic_set_mode(CU_BRM_VERBOSE);
@@ -68,4 +91,43 @@ void test_divide(void) {
   
   /* Dummy fail*/
   CU_ASSERT(3 == divide(2, 2));
+}
+
+void test_factorial(void){
+  CU_ASSERT(120==factorial(5));
+}
+
+void test_angstrom(void){
+  CU_ASSERT(1==isangstrom(153));
+}
+void test_prime(void){
+  CU_ASSERT(0==isprime(7));
+}
+void test_evenodd(void){
+  CU_ASSERT(1==even_odd(18));
+}
+
+void test_circlearea(void){
+  CU_ASSERT(314==circle_area(1));
+}
+void test_circlepm(void){
+  CU_ASSERT(6.28==circle_pm(1));
+}
+void test_squarearea(void){
+  CU_ASSERT(16==square_area(4));
+}
+void test_squarepm(void){
+  CU_ASSERT(16==square_pm(4));
+}
+void test_rectarea(void){
+  CU_ASSERT(18==rect_area(9,2));
+}
+void test_rectpm(void){
+  CU_ASSERT(22==rect_pm(9,2));
+}
+void test_triarea(void){
+  CU_ASSERT(27.712812==tri_area(8,8,8));
+}
+void test_tripm(void){
+  CU_ASSERT(24==tri_pm(8,8,8));
 }

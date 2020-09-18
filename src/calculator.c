dbe4b613b8aea595403c586fc4aa913cd5ff3b31
99002585 <71482380+99002585@users.noreply.github.com>
2020-09-18 23:57:28 +0530

    Update calculator.c

--- src/calculator.c
+++ src/calculator.c
@@ -1,4 +1,5 @@
 #include <calculator.h>
+#include <math.h>
 
 int add(int operand1, int operand2)
 {
@@ -39,3 +40,26 @@ int factorial(int operand1)
     return fact;
   }
 }
+
+int isangstrom(int operand1)
+{
+    int temp_operand,loop_var=0,remainder=0,result=0;
+    for (temp_operand = operand1; temp_operand != 0; ++loop_var)
+    {
+       temp_operand /= 10;
+    }
+
+   for (temp_operand = operand1; temp_operand != 0; temp_operand /= 10)
+   {
+       remainder = temp_operand % 10;
+
+      // store the sum of the power of individual digits in result
+      result += pow(remainder, loop_var);
+   }
+
+   // if num is equal to result, the number is an Armstrong number
+   if ((int)result == operand1)
+    return 1;
+   else
+    return 0;
+}    
